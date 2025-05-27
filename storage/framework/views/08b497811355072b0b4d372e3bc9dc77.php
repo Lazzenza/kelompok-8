86632f0c01262c75a94aee9ea319b793f5d64aec
CharlesThegoodguy <119509203+CharlesThegoodguy@users.noreply.github.com>
2025-05-27 13:54:50 +0700

    admin dan user

---
+++ storage/framework/views/08b497811355072b0b4d372e3bc9dc77.php
@@ -0,0 +1,12 @@
+<?php $__env->startSection('title','Home - Rent Car'); ?>
+
+<?php $__env->startSection('content'); ?>
+  <?php echo $__env->make('layout.card', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
+   <div class="container-fluid pt-4 px-4">
+      <div class="row g-4">
+        
+      </div>
+   </div>
+    
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layout.template', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\Users\ACER\ren_car\resources\views/home.blade.php ENDPATH**/ ?>
